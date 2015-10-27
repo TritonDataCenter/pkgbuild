366026f9dc7b96a7ebd4cfc4c9947669b6a82843
Jonathan Perkin <jperkin@joyent.com>
2015-10-27 10:59:32 +0000

    Add recent OSX changes.

--- include/osx.mk
+++ include/osx.mk
@@ -6,11 +6,21 @@
 .if !empty(PKGPATH:Mgraphics/rayshade) \
  || !empty(PKGPATH:Mlang/jamvm) \
  || !empty(PKGPATH:Mmail/elm) \
+ || !empty(PKGPATH:Mnet/gtk-gnutella) \
  || !empty(PKGPATH:Mnews/knews) \
  || !empty(PKGPATH:Mshells/pdksh) \
  || !empty(PKGPATH:Mtextproc/ja-groff) \
+ || !empty(PKGPATH:Mwip/oce) \
  || !empty(PKGPATH:Mwww/wml)
 NOT_FOR_BULK_PLATFORM=	Darwin-*-*
+.endif
+
+# Packages which do not fit in ramdisk build area.
+.if !empty(PKGPATH:Mcross/avr-gcc) \
+ || !empty(PKGPATH:Mgames/flightgear-data) \
+ || !empty(PKGPATH:Mlang/gcc*) \
+ || !empty(PKGPATH:Mlang/mercury)
+WRKOBJDIR=		/Users/pbulk/build-disk
 .endif
 
 # OSX does not perform name resolution in a chroot, so we need to hardcode
@@ -19,10 +29,16 @@ MASTER_SITE_OVERRIDE=	http://ftp.NetBSD.org/pub/pkgsrc/distfiles/
 
 # Fix GCC builds
 MULTILIB_SUPPORTED=	no
+PKGSRC_FORTRAN=		gfortran
 
-# Use XQuartz
+# X11 selection.  2015Q3 and earlier use Xquartz, 2015Q4 and onwards use
+# modular-xorg from pkgsrc.
+.if !empty(BRANCH:M*trunk)
+X11_TYPE=	modular
+.else
 X11_TYPE=	native
 X11BASE=	/opt/X11
+.endif
 
 # Native curl SIGBUS's when accessing HTTPS in a chroot.
 .if exists(${TOOLS_BASEDIR}/bin/curl)
