ae354211677fdc6fa1ee8def434ced13f43d7d43
Jonathan Perkin <jperkin@joyent.com>
2016-04-29 11:19:10 +0100

    Add hacks.mk

---
+++ include/hacks.mk
@@ -0,0 +1,15 @@
+#
+# Hacks for which there is currently no better place.  Eventually this file
+# should be empty and removed as things get fixed properly.
+#
+
+#
+# When enabling -gdwarf-2 we hit GNU ar limits on file size, resulting in:
+#
+#   ar: .libs/libwebkitgtk-1.0.a: File truncated
+#
+# For now, remove -g* for those packages to keep the file size down.
+#
+.if !empty(PKGPATH:Mwww/webkit-gtk*) || !empty(PKGPATH:Mwww/webkit24-gtk*)
+CFLAGS:=	${CFLAGS:C/-g.*//}
+.endif
