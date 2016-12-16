bf46a83fbea362ca415bf61aefeb349dcf0a72b7
Jonathan Perkin <jperkin@joyent.com>
2016-12-16 11:54:44 +0000

    joyent/pkgsrc#429 build vim with language support.

--- include/pkgoptions/osx.mk
+++ include/pkgoptions/osx.mk
@@ -18,6 +18,7 @@ PKG_OPTIONS.gnustep+=	fragile
 PKG_OPTIONS.gtk-vnc+=	-python # Allow this to build
 PKG_OPTIONS.nginx+=	dav status realip uwsgi memcache naxsi perl gzip
 PKG_OPTIONS.nginx+=	v2
+PKG_OPTIONS.vim+=	lua perl python ruby
 PKG_OPTIONS.weechat+=	lua perl python ruby wide-curses
 
 #
