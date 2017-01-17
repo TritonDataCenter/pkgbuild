884809325755a8eb8dd545b729e8258f4e5fe0d2
Jonathan Perkin <jperkin@joyent.com>
2017-01-17 15:59:22 +0000

    Enable http2 option by default (joyent/pkgsrc#451).

--- include/pkgoptions/osx.mk
+++ include/pkgoptions/osx.mk
@@ -4,7 +4,7 @@
 PKG_DEFAULT_OPTIONS+=	ncursesw
 PKG_DEFAULT_OPTIONS+=	unicode utf8
 PKG_DEFAULT_OPTIONS+=	bzip bzip2 lzo
-PKG_DEFAULT_OPTIONS+=	curl geoip inet6 libssh2
+PKG_DEFAULT_OPTIONS+=	curl geoip http2 inet6 libssh2
 PKG_DEFAULT_OPTIONS+=	openssl sasl ssl tls
 PKG_DEFAULT_OPTIONS+=	bdb db4 ldap
 PKG_DEFAULT_OPTIONS+=	unixodbc memcached
