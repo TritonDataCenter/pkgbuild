16cd0205b49f2ea07fddc7a8d7d3dae82cd71940
Jonathan Perkin <jperkin@joyent.com>
2017-08-02 09:34:41 +0100

    Split out USE_PKGSRC_GCC logic to a separate file.
    Simplify a few other things.  Note that the PKGSRC_GCC_REQD pkgbuild
    variable is now named PKGBUILD_GCC_REQD, it's unlikely anyone is using
    this but if so this is your notice.  Others will follow suit shortly.

---
+++ include/pkgsrc-gcc.mk
@@ -0,0 +1,27 @@
+#
+# Makefile fragment for using GCC from pkgsrc.  This fragment requires that
+# PKGBUILD_GCCBASE and PKGBUILD_GCC_REQD have been set prior to include.
+#
+
+#
+# Use external GCC for GCC itself and its dependencies, otherwise we run into
+# circular dependencies.
+#
+.if !empty(PKGPATH:Mdevel/binutils) \
+ || !empty(PKGPATH:Mdevel/gmake) \
+ || !empty(PKGPATH:Mdevel/nbpatch) \
+ || !empty(PKGPATH:Mlang/gcc2) \
+ || !empty(PKGPATH:Mlang/gcc3*) \
+ || !empty(PKGPATH:Mlang/gcc??) \
+ || !empty(PKGPATH:Mmisc/root) \
+ || !empty(PKGPATH:Mpkgtools/abiexec) \
+ || !empty(PKGPATH:Mpkgtools/cwrappers) \
+ || !empty(PKGPATH:Mpkgtools/digest) \
+ || !empty(PKGPATH:Mpkgtools/isaexec) \
+ || !empty(PKGPATH:Mpkgtools/pkg_install-info) \
+ || !empty(PKGPATH:Msysutils/checkperms)
+GCCBASE=	${PKGBUILD_GCCBASE}
+.else
+GCC_REQD=	${PKGBUILD_GCC_REQD}
+USE_PKGSRC_GCC=	yes
+.endif
