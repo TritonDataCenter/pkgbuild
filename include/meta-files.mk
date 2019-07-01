55b8da3bc0d1c5247834814b9688af1906422a0d
Jonathan Perkin <jperkin@joyent.com>
2019-07-01 11:44:26 +0100

    Use common INSTALL fragments where possible.

--- include/meta-files.mk
+++ include/meta-files.mk
@@ -3,16 +3,29 @@
 #
 
 #
+# Avoid gratuitous copies when they can be shared.
+#
+.if !empty(PKGPATH:M*/postgresql*server)
+PKGBUILD_META_DIR=	${PKGBUILD_BASEDIR}/meta/common/postgresql-server
+.elif !empty(PKGPATH:M*/mysql5[156]-server)
+PKGBUILD_META_DIR=	${PKGBUILD_BASEDIR}/meta/common/mysql5-server
+.elif !empty(PKGPATH:M*/mysql57-server) || !empty(PKGPATH:M*/mysql8*-server)
+PKGBUILD_META_DIR=	${PKGBUILD_BASEDIR}/meta/common/mysql8-server
+.else
+PKGBUILD_META_DIR=	${PKGBUILD_BASEDIR}/meta/${PKGPATH}
+.endif
+
+#
 # Pull in override INSTALL files.
 #
-.if exists(${PKGBUILD_BASEDIR}/meta/${PKGPATH}/INSTALL)
-INSTALL_TEMPLATES+=	${PKGBUILD_BASEDIR}/meta/${PKGPATH}/INSTALL
+.if exists(${PKGBUILD_META_DIR}/INSTALL)
+INSTALL_TEMPLATES+=	${PKGBUILD_META_DIR}/INSTALL
 .endif
 
 #
 # Pull in logadm INSTALL files (to be removed when logadm support is
 # added into pkgsrc).
 #
-.if exists(${PKGBUILD_BASEDIR}/meta/${PKGPATH}/INSTALL.logadm)
-INSTALL_TEMPLATES+=	${PKGBUILD_BASEDIR}/meta/${PKGPATH}/INSTALL.logadm
+.if exists(${PKGBUILD_META_DIR}/INSTALL.logadm)
+INSTALL_TEMPLATES+=	${PKGBUILD_META_DIR}/INSTALL.logadm
 .endif
