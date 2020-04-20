15bc9f46b5532877bf74dd08cb06254d3be206ad
Jonathan Perkin <jperkin@joyent.com>
2020-04-20 14:36:17 +0100

    Sync some multimedia PKG_OPTIONS to macOS.

--- include/pkgoptions/macos.mk
+++ include/pkgoptions/macos.mk
@@ -9,8 +9,9 @@ PKG_DEFAULT_OPTIONS+=	openssl sasl ssl tls
 PKG_DEFAULT_OPTIONS+=	bdb db4 ldap
 PKG_DEFAULT_OPTIONS+=	unixodbc memcached
 PKG_DEFAULT_OPTIONS+=	gif jpeg png rsvg
-PKG_DEFAULT_OPTIONS+=	faad lame mad vorbis
+PKG_DEFAULT_OPTIONS+=	faad lame mad vorbis jack
 PKG_DEFAULT_OPTIONS+=	dv opencore-amr theora x264 xvid
+PKG_DEFAULT_OPTIONS+=	opus x265
 #
 PKG_DEFAULT_OPTIONS+=	mutt-hcache mutt-smtp tokyocabinet
 #
