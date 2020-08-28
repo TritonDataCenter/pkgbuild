c6774ab8a2d6c23160b87726012cb1a8a9177479
Jonathan Perkin <jperkin@joyent.com>
2020-08-28 10:55:54 +0100

    Various fixes for the NetBSD builds.

--- include/netbsd.mk
+++ include/netbsd.mk
@@ -1,3 +1,17 @@
 #
 # Additional mk.conf settings specific to NetBSD.
 #
+
+#
+# Builds that currently hang the build.
+#
+.if !empty(PKGPATH:Mgames/craft)
+NOT_FOR_BULK_PLATFORM=	NetBSD-*-*
+.endif
+
+#
+# Builds too big for tmpfs.
+#
+.if !empty(PKGPATH:Mgames/flightgear*)
+WRKOBJDIR=	/Users/pbulk/build-disk
+.endif
