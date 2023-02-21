6b07c9a7a3567d7120df5622ba18511ff6850746
Jonathan Perkin <jonathan@perkin.org.uk>
2023-02-21 12:10:27 +0000

    Consolidate WRKOBJDIR disk-backed handling.
    Set up WRKOBJDIR_DISK on a per-build basis, and include wrkobjdir.mk for
    a common set of packages that will benefit from it.

--- include/macos.mk
+++ include/macos.mk
@@ -2,51 +2,6 @@
 # Additional mk.conf settings specific to macOS.
 #
 
-# Packages which do not fit in ramdisk build area.
-.if !empty(PKGPATH:Maudio/musescore) \
- || !empty(PKGPATH:Mcross/*gcc*) \
- || !empty(PKGPATH:Mdatabases/mariadb55-server) \
- || !empty(PKGPATH:Mdatabases/mongodb) \
- || !empty(PKGPATH:Mdatabases/prometheus) \
- || !empty(PKGPATH:Mdevel/xulrunner*) \
- || !empty(PKGPATH:Memulators/mame) \
- || !empty(PKGPATH:Memulators/qemu) \
- || !empty(PKGPATH:Mfonts/noto-ttf) \
- || !empty(PKGPATH:Mgames/flightgear-data) \
- || !empty(PKGPATH:Mgeography/proj) \
- || !empty(PKGPATH:Mgraphics/tesseract) \
- || !empty(PKGPATH:Mham/gnuradio-core) \
- || !empty(PKGPATH:Mjoyent/mariadb10-server) \
- || !empty(PKGPATH:Mlang/clang*) \
- || !empty(PKGPATH:Mlang/gcc*) \
- || !empty(PKGPATH:Mlang/ghc*) \
- || !empty(PKGPATH:Mlang/llvm) \
- || !empty(PKGPATH:Mlang/mercury) \
- || !empty(PKGPATH:Mlang/mono*) \
- || !empty(PKGPATH:Mlang/nodejs*) \
- || !empty(PKGPATH:Mlang/rust) \
- || !empty(PKGPATH:Mmail/thunderbird*) \
- || !empty(PKGPATH:Mnet/kubectl) \
- || !empty(PKGPATH:Mnet/rclone) \
- || !empty(PKGPATH:Mnet/terraform) \
- || !empty(PKGPATH:Msecurity/trufflehog) \
- || !empty(PKGPATH:Msecurity/vault) \
- || !empty(PKGPATH:Msysutils/consul) \
- || !empty(PKGPATH:Msysutils/restic) \
- || !empty(PKGPATH:Mwip/ghc) \
- || !empty(PKGPATH:Mwip/rust) \
- || !empty(PKGPATH:Mwww/firefox) \
- || !empty(PKGPATH:Mwww/firefox??) \
- || !empty(PKGPATH:Mwww/grafana) \
- || !empty(PKGPATH:Mwww/hugo) \
- || !empty(PKGPATH:Mwww/seamonkey) \
- || !empty(PKGPATH:Mwww/webkit*-gtk*) \
- || !empty(PKGPATH:Mx11/kdelibs4) \
- || !empty(PKGPATH:Mx11/qt4-libs) \
- || !empty(PKGPATH:Mx11/qt5-qtwebkit)
-WRKOBJDIR=		/Users/pbulk/build-disk
-.endif
-
 # /tmp/go-build* sometimes fills up
 MAKE_ENV+=		GOTMPDIR=${WRKOBJDIR:Q}
 
