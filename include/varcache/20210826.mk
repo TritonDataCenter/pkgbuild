775fc2ae3b822feb7fd3ca0fcc7e1d6606139e68
Jonathan Perkin <jperkin@smartos.org>
2025-01-02 11:47:08 +0000

    Update 20210826 varcache.
    This version uses the extra/varcache fake package to make it easier to
    see changes, and adds a bunch of new variables.

--- include/varcache/20210826.mk
+++ include/varcache/20210826.mk
@@ -5,158 +5,235 @@
 #
 # This file is specific to the 20210826 SmartOS build platform.
 #
-
+# Generated using 'bmake show-varcache' in extra/varcache, using pkgsrc trunk
+# from 20240513.
 #
-# General variables.
+AMDGPU_DRV=				__nonexistent__
+APM_DRV=				__nonexistent__
+ARK_DRV=				__nonexistent__
+AST_DRV=				__nonexistent__
+ATI_DRV=				__nonexistent__
+BINUTILS_FILES=				__nonexistent__
+BIN_FILE=				/usr/bin/file
+BUILTIN_LIB_FOUND.atomic=		no
+BUILTIN_LIB_FOUND.bind=			no
+BUILTIN_LIB_FOUND.c_r=			yes
+BUILTIN_LIB_FOUND.crypto=		yes
+BUILTIN_LIB_FOUND.curses=		yes
+BUILTIN_LIB_FOUND.db=			no
+BUILTIN_LIB_FOUND.db1=			no
+BUILTIN_LIB_FOUND.des=			no
+BUILTIN_LIB_FOUND.dl=			yes
+BUILTIN_LIB_FOUND.edit=			no
+BUILTIN_LIB_FOUND.history=		no
+BUILTIN_LIB_FOUND.iconv=		no
+BUILTIN_LIB_FOUND.intl=			yes
+BUILTIN_LIB_FOUND.milter=		no
+BUILTIN_LIB_FOUND.ncurses=		no
+BUILTIN_LIB_FOUND.ossaudio=		no
+BUILTIN_LIB_FOUND.pthread=		yes
+BUILTIN_LIB_FOUND.readline=		no
+BUILTIN_LIB_FOUND.rt=			yes
+BUILTIN_LIB_FOUND.ssl=			yes
+BUILTIN_LIB_FOUND.termcap=		yes
+BUILTIN_LIB_FOUND.terminfo=		no
+BUILTIN_LIB_FOUND.termlib=		yes
+BUILTIN_LIB_FOUND.tinfo=		no
+BUILTIN_LIB_FOUND.uuid=			yes
+BUILTIN_LIB_FOUND.wrap=			yes
+BUILTIN_PKG.dlcompat=			dlcompat-20030629
+BUILTIN_PKG.mit-krb5=			mit-krb5-1.6.3
+CHIPS_DRV=				__nonexistent__
+CIRRUS_DRV=				__nonexistent__
+DUMMY_DRV=				__nonexistent__
+ELOGRAPHICS_DRV=			__nonexistent__
+FBDEV_DRV=				__nonexistent__
+FIND_FILES_alsa-lib=			__nonexistent__
+FIND_FILES_atf=				__nonexistent__
+FIND_FILES_fuse=			__nonexistent__
+FIND_FILES_libFS=			__nonexistent__
+FIND_FILES_libX11=			__nonexistent__
+FIND_FILES_libXScrnSaver=		__nonexistent__
+FIND_FILES_libXdamage=			__nonexistent__
+FIND_FILES_libXext=			__nonexistent__
+FIND_FILES_libXfixes=			__nonexistent__
+FIND_FILES_libXfont=			__nonexistent__
+FIND_FILES_libXfont2=			__nonexistent__
+FIND_FILES_libXi=			__nonexistent__
+FIND_FILES_libXinerama=			__nonexistent__
+FIND_FILES_libXpresent=			__nonexistent__
+FIND_FILES_libXrandr=			__nonexistent__
+FIND_FILES_libXrender=			__nonexistent__
+FIND_FILES_libXres=			__nonexistent__
+FIND_FILES_libXtst=			__nonexistent__
+FIND_FILES_libXv=			__nonexistent__
+FIND_FILES_libdrm=			__nonexistent__
+FIND_FILES_libepoxy=			__nonexistent__
+FIND_FILES_libfontenc=			__nonexistent__
+FIND_FILES_libpciaccess=		__nonexistent__
+FIND_FILES_libusb=			__nonexistent__
+FIND_FILES_libvdpau=			__nonexistent__
+FIND_FILES_libxcb=			__nonexistent__
+FIND_FILES_libxshmfence=		__nonexistent__
+FIND_FILES_modular-xorg-server=		__nonexistent__
+FIND_FILES_mpcomplex=			__nonexistent__
+FIND_FILES_openldap-client=		__nonexistent__
+FIND_FILES_pixman=			__nonexistent__
+FIND_FILES_sqlite3=			__nonexistent__
+FIND_FILES_xbitmaps=			__nonexistent__
+FIND_FILES_xcb-proto=			__nonexistent__
+FIND_FILES_xcb-util=			__nonexistent__
+FIND_FILES_xkeyboard-config=		__nonexistent__
+FIND_FILES_xorgproto=			__nonexistent__
+FIND_FILES_xtrans=			__nonexistent__
+GDBM_H=					__nonexistent__
+GLINT_DRV=				__nonexistent__
+H_ARCHIVE=				__nonexistent__
+H_ARGP=					__nonexistent__
+H_BZIP2=				__nonexistent__
+H_CITRUS_ICONV=				__nonexistent__
+H_CURSES=				/usr/include/curses.h
+H_CURSES1=				__nonexistent__
+H_CURSES_CHGAT=				__nonexistent__
+H_CURSES_GETMOUSE=			/usr/include/curses.h
+H_CURSES_GETSYX=			/usr/include/curses.h
+H_CURSES_HALFDELAY=			/usr/include/curses.h
+H_CURSES_MVWCHGAT=			__nonexistent__
+H_CURSES_PUTWIN=			/usr/include/curses.h
+H_CURSES_RESIZETERM=			__nonexistent__
+H_CURSES_RESIZE_TERM=			__nonexistent__
+H_CURSES_RIPOFFLINE=			/usr/include/curses.h
+H_CURSES_SET_ESCDELAY=			__nonexistent__
+H_CURSES_SYNCOK=			/usr/include/curses.h
+H_CURSES_VW_PRINTW=			__nonexistent__
+H_CURSES_WA_NORMAL=			__nonexistent__
+H_CURSES_WGETNSTR=			/usr/include/curses.h
+H_CURSES_WSYNCUP=			/usr/include/curses.h
+H_DB=					__nonexistent__
+H_DL=					/usr/include/dlfcn.h
+H_DLFCN=				/usr/include/dlfcn.h
+H_DNSSD=				/usr/include/dns_sd.h
+H_EDITLINE=				__nonexistent__
+H_EXECINFO=				/usr/include/execinfo.h
+H_EXPAT=				__nonexistent__
+H_FAM=					__nonexistent__
+H_FILE=					__nonexistent__
+H_FONTCONFIG=				__nonexistent__
+H_FREETYPE2=				__nonexistent__
+H_FREETYPE2_NEW=			__nonexistent__
+H_GENTOO_GETTEXT=			__nonexistent__
+H_GETOPT=				/usr/include/getopt.h
+H_GETTEXT=				__nonexistent__
+H_GLIBC_ICONV=				__nonexistent__
+H_GLU=					__nonexistent__
+H_GMP=					__nonexistent__
+H_HEIMDAL=				__nonexistent__
+H_HESIOD=				__nonexistent__
+H_ICE=					__nonexistent__
+H_ICONV=				__nonexistent__
+H_INOTIFY=				/usr/include/sys/inotify.h
+H_LIBBSD=				__nonexistent__
+H_LIBDES=				__nonexistent__
+H_LIBEVENT=				__nonexistent__
+H_LIBEVENTCONFIG=			__nonexistent__
+H_LIBMD=				__nonexistent__
+H_LIBMILTER=				/usr/include/libmilter/mfapi.h
+H_LIBPCAP=				__nonexistent__
+H_LIBTIRPC=				__nonexistent__
+H_LINUX_PAM=				__nonexistent__
+H_LZMA=					__nonexistent__
+H_LZMA_VERSION=				__nonexistent__
+H_MESALIB=				__nonexistent__
+H_MIT_KRB5=				/usr/include/kerberosv5/krb5.h
+H_MPC=					__nonexistent__
+H_MPFR=					__nonexistent__
+H_NCURSES=				__nonexistent__
+H_NETSNMP=				__nonexistent__
+H_NGETTEXT_GETTEXT=			/usr/include/libintl.h
+H_OPENPAM=				__nonexistent__
+H_OPENSSLCONF=				__nonexistent__
+H_OPENSSLV=				__nonexistent__
+H_OPNSVR5_GETTEXT=			__nonexistent__
+H_PERFUSE=				__nonexistent__
+H_PTHREAD=				/usr/include/pthread.h
+H_READLINE=				__nonexistent__
+H_SKEY=					__nonexistent__
+H_SM=					__nonexistent__
+H_SOLARIS_PAM=				/usr/include/security/pam_appl.h
+H_SOUNDCARD=				/usr/include/sys/soundcard.h
+H_SYSEXITS=				/usr/include/sysexits.h
+H_TCP_WRAPPERS=				/usr/include/tcpd.h
+H_TERM=					/usr/include/term.h
+H_TERMCAP=				__nonexistent__
+H_TERMLIB=				__nonexistent__
+H_TERM_TC=				/usr/include/term.h
+H_UUID=					/usr/include/uuid/uuid.h
+H_XAUTH=				__nonexistent__
+H_XCOMPOSITE=				__nonexistent__
+H_XCURSOR=				__nonexistent__
+H_XDMCP=				__nonexistent__
+H_XFT2=					__nonexistent__
+H_XINPUT=				__nonexistent__
+H_XMU=					__nonexistent__
+H_XPM=					__nonexistent__
+H_XRANDR=				__nonexistent__
+H_XTOS=					__nonexistent__
+H_ZLIB=					__nonexistent__
+I128_DRV=				__nonexistent__
+I740_DRV=				__nonexistent__
+INTEL_DRV=				__nonexistent__
+JOYSTICK_DRV=				__nonexistent__
+KEYBOARD_DRV=				__nonexistent__
+LIBUDEV_PC=				__nonexistent__
+MACH64_DRV=				__nonexistent__
+MGA_DRV=				__nonexistent__
+MOUSE_DRV=				__nonexistent__
+NCURSES_PC=				__nonexistent__
+NDBM_H=					__nonexistent__
+NEOMAGIC_DRV=				__nonexistent__
+NOUVEAU_DRV=				__nonexistent__
+NV_DRV=					__nonexistent__
+OPENCHROME_DRV=				__nonexistent__
+PC_GL=					__nonexistent__
+PC_GLU=					__nonexistent__
+PC_LIBNSL=				__nonexistent__
+PC_LIBUSB1=				__nonexistent__
+PC_LIBXVMC=				__nonexistent__
+QXL_DRV=				__nonexistent__
+R128_DRV=				__nonexistent__
+RENDITION_DRV=				__nonexistent__
+S3VIRAGE_DRV=				__nonexistent__
+S3_DRV=					__nonexistent__
+SAVAGE_DRV=				__nonexistent__
+SH_KRB5_CONFIG=				/usr/bin/krb5-config
+SILICONMOTION_DRV=			__nonexistent__
+SIS_DRV=				__nonexistent__
+SUNCG14_DRV=				__nonexistent__
+SUNCG3_DRV=				__nonexistent__
+SUNCG6_DRV=				__nonexistent__
+SUNFFB_DRV=				__nonexistent__
+SUNLEO_DRV=				__nonexistent__
+SUNTCX_DRV=				__nonexistent__
+TDFX_DRV=				__nonexistent__
+TGA_DRV=				__nonexistent__
+TRIDENT_DRV=				__nonexistent__
+TSENG_DRV=				__nonexistent__
+UUID_PC=				__nonexistent__
+VBOXVIDEO_DRV=				__nonexistent__
+VESA_DRV=				__nonexistent__
+VMMOUSE_DRV=				__nonexistent__
+VMWARE_DRV=				__nonexistent__
+VOID_DRV=				__nonexistent__
+VOODOO_DRV=				__nonexistent__
+WSFB_DRV=				__nonexistent__
+WS_DRV=					__nonexistent__
+XAUTH=					__nonexistent__
+XINIT=					__nonexistent__
+_BLTN_H_READLINE=			__nonexistent__
 #
-OPSYS=				SunOS
-OS_VERSION=			5.11
-OPSYS_VERSION=			051100
-
-# archivers/bzip2/builtin.mk
-H_BZIP2=			__nonexistent__
-
-# archivers/libarchive/builtin.mk
-H_ARCHIVE=			__nonexistent__
-
-# archivers/xz/builtin.mk
-H_LZMA=				__nonexistent__
-H_LZMA_VERSION=			__nonexistent__
-
-# converters/libiconv/builtin.mk
-BUILTIN_LIB_FOUND.iconv=	no
-H_ICONV=			__nonexistent__
-H_GLIBC_ICONV=			__nonexistent__
-H_CITRUS_ICONV=			__nonexistent__
-
-# databases/gdbm/builtin.mk
-GDBM_H=				__nonexistent__
-
-# devel/editline/builtin.mk
-BUILTIN_LIB_FOUND.edit=		no
-H_EDITLINE=			__nonexistent__
-
-# devel/gettext-lib/builtin.mk
-BUILTIN_LIB_FOUND.intl=		yes
-H_GETTEXT=			__nonexistent__
-H_GENTOO_GETTEXT=		__nonexistent__
-H_NGETTEXT_GETTEXT=		/usr/include/libintl.h
-H_OPNSVR5_GETTEXT=		__nonexistent__
-
-# devel/gmp/builtin.mk
-H_GMP=				__nonexistent__
-
-# devel/libevent/builtin.mk
-H_LIBEVENT=			__nonexistent__
-H_LIBEVENTCONFIG=		__nonexistent__
-
-# devel/libexecinfo/builtin.mk
-H_EXECINFO=			/usr/include/execinfo.h
-
-# devel/libgetopt/builtin.mk
-H_GETOPT=			/usr/include/getopt.h
-
-# devel/libuuid/builtin.mk
-BUILTIN_LIB_FOUND.uuid=		yes
-H_UUID=				/usr/include/uuid/uuid.h
-
-# devel/ncurses/builtin.mk
-# devel/ncursesw/builtin.mk
-BUILTIN_LIB_FOUND.ncurses=	no
+# Added manually for LTS or missing MAKEVARS.
+#
 BUILTIN_LIB_FOUND.ncursesw=	no
-H_NCURSES=			__nonexistent__
+BUILTIN_PKG.mDNSResponder=	mDNSResponder-0
 H_NB_CURSESW=			__nonexistent__
 H_NCURSESW=			__nonexistent__
-H_CURSES1=			__nonexistent__
-
-# devel/sysexits/builtin.mk
-H_SYSEXITS=			/usr/include/sysexits.h
-
-# devel/zlib/builtin.mk
-H_ZLIB=				__nonexistent__
-
-# fonts/fontconfig/builtin.mk
-H_FONTCONFIG=			__nonexistent__
-
-# mail/libmilter/builtin.mk
-BUILTIN_LIB_FOUND.milter=	no
-H_LIBMILTER=			/usr/include/libmilter/mfapi.h
-
-# mk/curses.builtin.mk
-BUILTIN_LIB_FOUND.curses=	yes
-H_CURSES=			/usr/include/curses.h
-H_CURSES_CHGAT=			__nonexistent__
-H_CURSES_GETSYX=		/usr/include/curses.h
-H_CURSES_HALFDELAY=		/usr/include/curses.h
-H_CURSES_PUTWIN=		/usr/include/curses.h
-H_CURSES_RESIZE_TERM=		__nonexistent__
-H_CURSES_RESIZETERM=		__nonexistent__
-H_CURSES_RIPOFFLINE=		/usr/include/curses.h
-H_CURSES_SET_ESCDELAY=		__nonexistent__
-H_CURSES_SYNCOK=		/usr/include/curses.h
-H_CURSES_WGETNSTR=		/usr/include/curses.h
-H_CURSES_WSYNCUP=		/usr/include/curses.h
-H_CURSES_MVWCHGAT=		__nonexistent__
-H_CURSES_VW_PRINTW=		__nonexistent__
-H_CURSES_GETMOUSE=		/usr/include/curses.h
-H_CURSES_WA_NORMAL=		__nonexistent__
-
-# mk/db1.builtin.mk
-BUILTIN_LIB_FOUND.db1=		no
-BUILTIN_LIB_FOUND.db=		no
-H_DB=				__nonexistent__
-
-# mk/dlopen.builtin.mk
-BUILTIN_LIB_FOUND.dl=		yes
-H_DL=				/usr/include/dlfcn.h
-
-# mk/linux-pam.builtin.mk
-H_LINUX_PAM=			__nonexistent__
-
-# mk/oss.builtin.mk
-BUILTIN_LIB_FOUND.ossaudio=	no
-H_SOUNDCARD=			/usr/include/sys/soundcard.h
-
-# mk/pthread.builtin.mk
-BUILTIN_LIB_FOUND.pthread=	yes
-BUILTIN_LIB_FOUND.c_r=		yes
-BUILTIN_LIB_FOUND.rt=		yes
-H_PTHREAD=			/usr/include/pthread.h
-
-# mk/readline.builtin.mk
-#  - minus duplicates from devel/editline
-BUILTIN_LIB_FOUND.readline=	no
-BUILTIN_LIB_FOUND.history=	no
-H_READLINE=			__nonexistent__
-
-# mk/solaris-pam.builtin.mk
-H_SOLARIS_PAM=			/usr/include/security/pam_appl.h
-
-# mk/termcap.builtin.mk
-# mk/terminfo.builtin.mk
-#   - minus duplicates from mk/curses.builtin.mk
-BUILTIN_LIB_FOUND.terminfo=	no
-BUILTIN_LIB_FOUND.termcap=	yes
-BUILTIN_LIB_FOUND.termlib=	yes
-BUILTIN_LIB_FOUND.tinfo=	no
-H_TERM=				/usr/include/term.h
-H_TERMCAP=			__nonexistent__
-H_TERMLIB=			__nonexistent__
-
-# net/mDNSResponder/builtin.mk
-BUILTIN_PKG.mDNSResponder=	mDNSResponder-0
-
-# security/mit-krb5/builtin.mk
-BUILTIN_PKG.mit-krb5=		mit-krb5-1.6.3
-H_MIT_KRB5=			/usr/include/kerberosv5/krb5.h
-SH_KRB5_CONFIG=			/usr/bin/krb5-config
-
-# security/openssl/builtin.mk
-BUILTIN_LIB_FOUND.crypto=	yes
-BUILTIN_LIB_FOUND.ssl=		yes
-H_OPENSSLCONF=			__nonexistent__
-H_OPENSSLV=			__nonexistent__
-
-# security/tcp_wrappers/builtin.mk
-BUILTIN_LIB_FOUND.wrap=		yes
-H_TCP_WRAPPERS=			/usr/include/tcpd.h
